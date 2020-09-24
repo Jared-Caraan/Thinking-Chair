5fc3155f04fcd2e84c5262f4d0de6feb8ab117a7
Jared Dominic V. Caraan <j.caraan04@gmail.com>
2020-09-24 21:40:34 +0800

    created delete_files.py

---
+++ delete_files.py
@@ -0,0 +1,21 @@
+import datetime as dt
+import time
+import os
+
+def del_zip(directory):
+    
+    for folder, subfolders, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.zip'):
+                path = os.path.join(folder,file)
+                info = os.stat(path)
+                mtime = dt.datetime.fromtimestamp(info.st_mtime)
+                if mtime <= (dt.datetime.now() - dt.timedelta(minutes = 25)):
+                    os.remove(os.path.join(folder,file))
+
+def main():
+    
+    del_zip(r'C:\Users\jcaraan\OneDrive - DXC Production\Desktop\test')
+
+if __name__ == "__main__":
+    main()
