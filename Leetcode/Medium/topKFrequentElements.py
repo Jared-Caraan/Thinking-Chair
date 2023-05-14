e9d2a1fe709f15c8edd494cdf5a16d2fea196dc8
Jared <jcaraan@archgroup.com>
2023-05-14 21:29:58 +0800

    add topKFrequentElements.py

--- Leetcode/Medium/topKFrequentElements.py
+++ Leetcode/Medium/topKFrequentElements.py
@@ -11,4 +11,19 @@ def topKFrequent(nums, k: int):
         
         return list(countNums.keys())[:k]
 
+        # count = {}
+        # freq = [[] for i in range(len(nums) + 1)]
+
+        # for n in nums:
+        #     count[n] = 1 + count.get(n, 0)
+        # for n, c in count.items():
+        #     freq[c].append(n)
+
+        # res = []
+        # for i in range(len(freq) - 1, 0, -1):
+        #     for n in freq[i]:
+        #         res.append(n)
+        #         if len(res) == k:
+        #             return res
+
 print(topKFrequent([1,1,1,2,2,3,4,5,5,5,5], 2))
