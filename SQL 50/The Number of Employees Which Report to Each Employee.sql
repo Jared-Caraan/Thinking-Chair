1e5f1a53df660c457cc84be9d303b4a971a3f5f7
Jared <j.caraan04@gmail.com>
2023-09-17 14:53:35 +0800

    SQL 50

---
+++ SQL 50/The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,63 @@
+"""
+Table: Employees
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| employee_id | int      |
+| name        | varchar  |
+| reports_to  | int      |
+| age         | int      |
++-------------+----------+
+employee_id is the primary key for this table.
+This table contains information about the employees and the id of the manager they report to. Some employees do not report to anyone (reports_to is null). 
+ 
+
+For this problem, we will consider a manager an employee who has at least 1 other employee reporting to them.
+
+Write an SQL query to report the ids and the names of all managers, the number of employees who report directly to them, and the average age of the reports rounded to the nearest integer.
+
+Return the result table ordered by employee_id.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employees table:
++-------------+---------+------------+-----+
+| employee_id | name    | reports_to | age |
++-------------+---------+------------+-----+
+| 9           | Hercy   | null       | 43  |
+| 6           | Alice   | 9          | 41  |
+| 4           | Bob     | 9          | 36  |
+| 2           | Winston | null       | 37  |
++-------------+---------+------------+-----+
+Output: 
++-------------+-------+---------------+-------------+
+| employee_id | name  | reports_count | average_age |
++-------------+-------+---------------+-------------+
+| 9           | Hercy | 2             | 39          |
++-------------+-------+---------------+-------------+
+Explanation: Hercy has 2 people report directly to him, Alice and Bob. Their average age is (41+36)/2 = 38.5, which is 39 after rounding it to the nearest integer.
+"""
+
+SELECT
+    e.employee_id,
+    e.name,
+    f.cnt AS reports_count,
+    f.age AS average_age
+FROM Employees e
+INNER JOIN (
+    SELECT
+        reports_to,
+        COUNT(employee_id) AS cnt,
+        ROUND(AVG(age)) AS age
+    FROM Employees
+    GROUP BY 1
+    HAVING reports_to IS NOT NULL
+) f
+ON e.employee_id = f.reports_to
+ORDER BY 1;
