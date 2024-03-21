f43fb86e91f26a50e1827dfa763a3cb1dc1a4b97
Jared <j.caraan04@gmail.com>
2024-03-22 06:00:11 +0800

    tried Car Fleet

--- Neetcode/Stack/Medium/Car Fleet.py
+++ Neetcode/Stack/Medium/Car Fleet.py
@@ -1,37 +1,39 @@
 def carFleet(target: int, position, speed) -> int:
-    counter = 0
-    while counter < 5:
-        for i in [1,2,3]:
-            print(i, end="")
-        counter += 1
-    # counter = 1
-    # holder = []
 
-    # for i, v in enumerate(position):
-    #     holder.append((v, speed[i]))
+    stack_position = []
+    stack_speed = []
+    target_locked = False
+
+    while len(position) != 1 and not target_locked:         # end if only one remains, or when everyone reaches the target
+        for ind, ele in enumerate(position):
+            if not stack_position:                          # if stack is empty
+                stack_position.append(ele + speed[ind])
+                stack_speed.append(speed[ind])
+            else:
+                if stack_position[-1] == ele + speed[ind]:  # if current is equal to the top
+                    if speed[ind] < stack_speed[-1]:        # just compare the speeds, pick the lesser one
+                        stack_speed.pop()
+                        stack_speed.append(speed[ind])
+                else:
+                    stack_position.append(ele + speed[ind])
+                    stack_speed.append(speed[ind])
+        
+        for i in position:                                  # check if all cars reached the targets to end the loop
+            if i < target:
+                target_locked = False
+                break
+            target_locked = True
+
+        position = stack_position                           # update position and speed
+        speed = stack_speed
+        stack_position = []
+        stack_speed = []
     
-    # holder = sorted(holder)
-
-    # for i in range(0, len(holder)):
-    #     if i != 1 and holder[i-1][1] <= holder[i][1]:
-    #         counter += 1
-
-    # return counter
-    # counter = 1
-    # holder = []
-    # stack = []
-
-    # for i, v in enumerate(position):
-    #     holder.append((v, speed[i]))
-    
-    # holder = sorted(holder)
-
-    # for i in range(0, len(holder)):
-    #     if i != 1 and holder[i-1][1] <= holder[i][1]:
-    #         counter += 1
-        # else:
-        #     stack.append(holder[i][0] + holder[i][1])
-
-    # return counter
+    return len(position)
 
 print(carFleet(10, [6,8], [3,2]))
+
+'''
+EDGE CASES
+10, [6,8], [3,2] -
+'''
